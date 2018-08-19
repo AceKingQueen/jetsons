edfbf5ef426986f4ecf428829f85cd9a10bd3854
Allison <allisonkingwilliams@gmail.com>
2018-08-19 14:34:17 -0400

    data table working on button click

--- javascript.js
+++ javascript.js
@@ -23,46 +23,38 @@ var table_data = [ { first_name : 'Rose',
                      last_name  : 'Foreman',
                      home       : 'Gallifrey'} ];
 
-console.log(table_data[0]);    
-
-
+//when the "See Data" button is clicked, run the populateTable function
 $("#add-data").on("click", function(event) {
 
     populateTable();
 
 });
 
+
 function populateTable() {
 
-    //create a new row and new cell for each item item in the array
-    var createRow = $("<tr></tr>");
-   
+    //loop over each object in the array, and for each one of them, create a new row
     table_data.forEach(function(item) {
 
-    var createCell = $("<td></td>").append(item.first_name);
+    //create individual cells for each piece of data    
+    var createFirstNameCell = $("<td></td>").append(item.first_name);
+    var createLastNameCell = $("<td></td>").append(item.last_name);
+    var createHomeCell = $("<td></td>").append(item.home);
+
+     //create a new row for each object item in the array
+    var createRow = $("<tr></tr>");
+
+    //append all the cells together
+    createRow.append(createFirstNameCell);
+    createRow.append(createLastNameCell);
+    createRow.append(createHomeCell);
     
-    createRow.append(createCell);
-    
+    //create the new row at the end of the table
+    $("#data-table").append(createRow);
+
     });
 
-    $("#data-table").append(createRow);
 }
 
-//loop over each object in the table_data array 
 
-
-    //display the corresponding object at the populate divs in the html
-//     $(".populate-first-name").append("<div>" + item.first_name + "</div>");
-
-    
-
-
-
-// table_data.forEach(function(item) {
-//     $(".populate-last-name").append("<div>" + item.last_name + "</div>");
-// });
-
-// table_data.forEach(function(item) {
-//     $(".populate-home").append("<div>" + item.home + "</div>");
-// });
 
