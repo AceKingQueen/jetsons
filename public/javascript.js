ee2baa31395c15c68bf01f43cfc81316bb836ee7
Allison <allisonkingwilliams@gmail.com>
2018-08-19 16:50:55 -0400

    add jumbotron

--- public/javascript.js
+++ public/javascript.js
@@ -25,7 +25,6 @@ var table_data = [ { first_name : 'Rose',
 
 //when the "See Data" button is clicked, run the populateTable function
 $("#add-data").on("click", function(event) {
-  console.log("button check");
 
     populateTable();
 
