de4b7c2a1e75f71712911268a55e0904ed9d4c62
Allison <allisonkingwilliams@gmail.com>
2018-08-27 22:31:20 -0400

    dynamic port needed on server.js for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
